0df5d5a18a61d27265f763569f655babb1f301cb
Jaewook Lee <jaewooklee042@gmail.com>
2022-07-08 06:17:15 +0200

    add dtype dict

--- napf/__init__.py
+++ napf/__init__.py
@@ -1,12 +1,14 @@
 from napf import _napf
 from napf import _napf as core
 from napf import base
-from napf.base import (validate_metric_input,
+from napf.base import (np2napf_dtypes,
+                       validate_metric_input,
                        core_class_str,
                        KDT)
 
 __all__ = [
     "core",
+    "np2napf_dtypes",
     "validate_metric_input",
     "core_class_str",
     "KDT",
