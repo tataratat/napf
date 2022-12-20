f55227a96b3e2439231b6950eb2de64217ce7d01
Jaewook Lee <jaewooklee042@gmail.com>
2022-12-20 08:43:20 +0100

    extend classifiers

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Natural Language :: English',
         'Topic :: Scientific/Engineering'
     ],
