ae090e3c6e9f2dc21b490de4ed829e9613baa68f
Jaewook Lee <jaewooklee042@gmail.com>
2023-06-23 11:22:25 +0200

    choose usable max threads

--- src/python/threadhelper.hpp
+++ src/python/threadhelper.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cmath>
+#include <algorithm>
 #include <thread>
 
 namespace napf {
@@ -9,22 +9,33 @@ namespace napf {
 template<typename Func, typename IndexT>
 void nthread_execution(Func& f, IndexT& total, IndexT& nthread) {
   // if nthread == 1, don't even bother creating thread
-  if (nthread == 1) {
+  if (nthread == 1 || nthread == 0) {
     f(0, total);
     return;
   }
 
-  // get chunk size and prepare threads
-  const IndexT chunk_size = (total + nthread - 1) / nthread;
-  std::vector<std::thread> tpool;
-  tpool.reserve(nthread);
+  int n_usable_threads{nthread};
 
-  for (int i{0}; i < (nthread - 1); i++) {
+  // negative input looks for hardware_concurrency
+  if (nthread < 0) {
+    n_usable_threads = std::max(std::thread::hardware_concurrency(), 1u);
+  }
+
+  // thread shouldn't exceed total
+  n_usable_threads = std::min(total, n_usable_threads);
+
+  // get chunk size and prepare threads
+  const IndexT chunk_size = (total + n_usable_threads - 1) / n_usable_threads;
+  std::vector<std::thread> tpool;
+  tpool.reserve(n_usable_threads);
+
+  for (int i{0}; i < (n_usable_threads - 1); i++) {
     tpool.emplace_back(std::thread{f, i * chunk_size, (i + 1) * chunk_size});
   }
   {
     // last one
-    tpool.emplace_back(std::thread{f, (nthread - 1) * chunk_size, total});
+    tpool.emplace_back(
+        std::thread{f, (n_usable_threads - 1) * chunk_size, total});
   }
 
   for (auto& t : tpool) {
