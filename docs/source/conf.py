6109750e037f0ed5ea39ed26345d69b4ae5607d8
Jaewook Lee <jaewooklee042@gmail.com>
2023-10-03 15:10:54 +0200

    add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import napf
+
+# sys.path.insert(0, os.path.abspath("../../"))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "napf"
+copyright = "2022, Jaewook Lee"
+author = "Jaewook Lee"
+release = napf.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_mdinclude",
+]
+
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+pygments_style = "sphinx"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+
+autodoc_default_options = {
+    "autosummary": True,
+}
+
+autosummary_context = {
+    "skipmethods": ["__init__"],
+}
